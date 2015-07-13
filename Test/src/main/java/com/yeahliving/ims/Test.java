9e62b008917d444a94e92209bdf5ea87eeb149f0
xingfeiy <yxfly@hotmail.com>
2015-07-13 14:57:23 -0700

    Added a simple class to test the workspace.

--- Test/src/main/java/com/yeahliving/ims/Test.java
+++ Test/src/main/java/com/yeahliving/ims/Test.java
@@ -6,5 +6,6 @@ package com.yeahliving.ims;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
